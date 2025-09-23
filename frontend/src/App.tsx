20ccb31112d0f2fa4a100958908bd5463adf8013
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-09-23 07:45:43 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { QRCodeSVG } from "qrcode.react"
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import phoneIcon from './assets/iphone.png'
+import computerIcon from './assets/computer.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("http://localhost:8080")
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header'>
+        <QRCodeSVG value={url}/>
+        <div className="detail">
+          <h3>{url}</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="device-list">
+        <div className="device-item">
+          <img src={phoneIcon} alt="" />
+          <div className="device-detail">
+            <h3>Device Name</h3>
+            <span>IP: 192.168.1.1</span>
+          </div>
+        </div>
+        <div className="device-item">
+          <img src={computerIcon} alt="" />
+          <div className="device-detail">
+            <h3>Device Name</h3>
+            <span>IP: 192.168.1.1</span>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
