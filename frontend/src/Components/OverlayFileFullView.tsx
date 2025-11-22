e7f0b73b63e341cb9eae656c4243edd96356e099
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-11-23 01:01:52 +0700

    update

--- frontend/src/Components/OverlayFileFullView.tsx
+++ frontend/src/Components/OverlayFileFullView.tsx
@@ -1,32 +1,35 @@
+import '../style/components/OverlayFileFullView.css'
+
 import leftArrowIcon from "../assets/left-arrow-white.png"
 import rightArrowIcon from "../assets/right-arrow-white.png"
-import imgTest from "../assets/test1.jpg"
+
+
 
 export interface OverlayFileFullViewModel {
-    fileId: string;
-    filePath: string;
+  fileId: string;
+  filePath: string;
 }
 
 type Props = {
-    file?: OverlayFileFullViewModel
-    onClick?: () => void
+  file?: OverlayFileFullViewModel
+  onClick?: () => void
 }
 
-export default function OverlayFileFullView({file, onClick}: Props) {
+export default function OverlayFileFullView({ file, onClick }: Props) {
 
-    return (
-        <div className="overlay-fixed">
-            <div className="overlay-content">
-                <div className="btn-overlay btn-prev"><img src={leftArrowIcon} alt="" /></div>
-                <div className="overlay-img-index-content" onClick={(e)=> {
-                  e.stopPropagation();
-                  onClick && onClick()
-                }}>
-                    <img className='overlay-img-index' src={file?.filePath} alt="" />
-                </div>
-                <div className="btn-overlay btn-next"><img src={rightArrowIcon} alt="" /></div>
-            </div>
-            {/* <div className="overlay-img-list">
+  return (
+    <div className="overlay-fixed">
+      <div className="overlay-content">
+        <div className="btn-overlay btn-prev"><img src={leftArrowIcon} alt="" /></div>
+        <div className="overlay-img-index-content" onClick={(e) => {
+          e.stopPropagation();
+          onClick && onClick()
+        }}>
+          <img className='overlay-img-index' src={file?.filePath} alt="" />
+        </div>
+        <div className="btn-overlay btn-next"><img src={rightArrowIcon} alt="" /></div>
+      </div>
+      {/* <div className="overlay-img-list">
           {files
             .filter(item => item.file_type.startsWith("image/"))
             .map((file, i) => (
@@ -39,6 +42,6 @@ export default function OverlayFileFullView({file, onClick}: Props) {
               </div>
             ))}
         </div> */}
-        </div>
-    )
+    </div>
+  )
 }
