4496ebf42c66f9939436c37ff5db8ca86ff6ffd5
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2026-01-03 13:42:51 +0700

    update

--- frontend/src/Components/FileInfo.tsx
+++ frontend/src/Components/FileInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useShared } from '@/contexts/SharedContext'
 
 import '@/style/components/file-info.css'
@@ -5,15 +6,19 @@ import '@/style/components/file-info.css'
 import { getDateTimeString } from '@/util/dateConvert';
 import FileCategory from '@/util/fileCategory';
 import fileSize from '@/util/fileSizeCalc';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+
+import editIcon from '@/assets/edit.png';
 
 export default function FileInfo() {
 
-    const { 
+    const {
         selectedFile,
         isSelectFile,
         isShowFileInfo
     } = useShared();
+
+    const [isEditingAccessScope, setIsEditingAccessScope] = useState(false);
 
     const colorTag = (tag?: string) => {
         switch (tag) {
@@ -26,11 +31,11 @@ export default function FileInfo() {
         }
     };
 
-    useEffect(() => {
-        console.log("Selected File Info: ", selectedFile);
-        console.log("isSelectFile: ", isSelectFile);
-        console.log("isShowFileInfo: ", isShowFileInfo);
-    }, [selectedFile]);
+    // useEffect(() => {
+    //     console.log("Selected File Info: ", selectedFile);
+    //     console.log("isSelectFile: ", isSelectFile);
+    //     console.log("isShowFileInfo: ", isShowFileInfo);
+    // }, [selectedFile]);
 
 
     return isSelectFile && isShowFileInfo && selectedFile ? (
@@ -67,7 +72,25 @@ export default function FileInfo() {
                 </div>
                 <div className="meta-data-item">
                     <p>Access Level</p>
-                    <p className={`card-text ${colorTag(selectedFile?.access_scope)}`}>{selectedFile?.access_scope}</p>
+                    <div className='row'>
+                        {
+                            isEditingAccessScope ? (
+                                <>
+                                    <select name="" id="" className='file-info-selector'>
+                                        <option value="PUBLIC">PUBLIC</option>
+                                        <option value="PRIVATE">PRIVATE</option>
+                                        <option value="PROTECTED">PROTECTED</option>
+                                    </select>
+                                    <input type="button" value="Save" className='file-info-btn-save-access-scope' onClick={() => setIsEditingAccessScope(false)}/>
+                                </>
+                            ) : (
+                                <>
+                                    <p className={`card-text ${colorTag(selectedFile?.access_scope)}`}>{selectedFile?.access_scope}</p>
+                                    <img src={editIcon} className='small' alt="" onClick={() => setIsEditingAccessScope(!isEditingAccessScope)} />
+                                </>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </div>
